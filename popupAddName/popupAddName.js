6cb1c5b274d14bb9fe60f9da57eba2071bca74ce
Petre Cosmin Badescu <badescupetrecosmin@gmail.com>
2020-08-29 18:44:03 +0300

    'changes'

--- popupAddName/popupAddName.js
+++ popupAddName/popupAddName.js
@@ -2,7 +2,23 @@ $(document).ready(function () {
     var max_fields = 10;
     var wrapper = $(".inner-wrapper");
     var add_button = $(".add");
-    var x = 1; 
+    var x = 1;
+    var inputs = [{
+        id: 'name',
+        fieldLabel: 'Name',
+        required: true,
+        type: 'text',
+        minLength: '3',
+        allowedCharacters: 'Alpha'
+    },
+    {
+        id: 'birthday',
+        fieldLabel: 'Birthday',
+        required: true,
+        type: 'text',
+        minLength: '3',
+        charactersLength: 8
+    }];
     $(add_button).click(function (e) {
         e.preventDefault();
         if (x < max_fields) {
@@ -10,54 +26,67 @@ $(document).ready(function () {
             $(wrapper).append(`
             <div class="inner-wrapper">
                 <div class="input-container">
-                    <input type="text" class="input-style" placeholder="Prenume" id="firstName${x}" />
+                    <input type="text" class="input-style" placeholder="Nume" id="name${x}" />
                     <div class="error-input-container">
                         <span class="error-validation"></span>
                     </div>
                 </div>
                 <div class="input-container">
-                    <input type="text" class="input-style" placeholder="Nume" id="lastName${x}" />
+                    <input type="tel" class="input-style" placeholder="Birthday (dd/mm/yy) " id="birthday${x}" />
                     <div class="error-input-container">
                         <span class="error-validation"></span>
                     </div>
                 </div>
-                <div class="remove_field text-right">Remove</div>
+                <span class="remove_field text-right">Remove</span>
             </div>
             `);
-        }
+            inputs.push({
+                id:`name${x}`,
+                fieldLabel: 'Name',
+                required: true,
+                type: 'text',
+                minLength: '3',
+                allowedCharacters: 'Alpha'
+            },
+            {
+                id:`birthday${x}`,
+                fieldLabel: 'Birthday',
+                required: true,
+                type: 'text',
+                minLength: '3',
+                charactersLength: 8
+            });
+           
+        };
+        setInput();
     });
     $(wrapper).on("click", ".remove_field", function (e) {
         e.preventDefault();
         $(this).parent('div').remove();
         x--;
+        const newInputs = inputs.slice(-1,1);
+        inputs = [...newInputs];
     });
 
-    (function setInput() {
-
+    function setInput() {
+        if(inputs.length > 2){
+            for(i=0; i< inputs.length/2 -1 ; i++) {
+                inputRestrictor.setInputOnlyWithDigits(`birthday${x}`);
+                inputRestrictor.setInputWithExactCharactersLength(`birthday${x}`, 8);
+                inputRestrictor.putCharacterAfterNumberOfCharacters(`birthday${x}`, "/", 2);
+                inputRestrictor.setDate(`birthday${x}`); 
+            }
+        }
+        inputRestrictor.setInputOnlyWithDigits("birthday");
         inputRestrictor.setInputWithExactCharactersLength("birthday", 8);
         inputRestrictor.putCharacterAfterNumberOfCharacters("birthday", "/", 2);
+        inputRestrictor.setDate("birthday");
         // step1 inputs
-        var inputs = [{
-            id: 'name',
-            fieldLabel: 'Name',
-            required: true,
-            type: 'text',
-            minLength: '3',
-            allowedCharacters: 'Alpha'
-        },
-        {
-            id: 'birthday',
-            fieldLabel: 'Birthday',
-            required: true,
-            type: 'text',
-            minLength: '3',
-            charactersLength: 8
-        }]
-   
         var inputsValidator = new FormValidator({ inputsList: inputs });
 
         $("#btn-cta").on("click", function () {
             generalMethods.goNextStep(inputsValidator);
         });
-    })();
+    };
+    setInput();
 });
